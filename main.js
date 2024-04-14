04ba50a3b382b6d834318a6feb027a4a7e8cfc42
Nikhil Sajwan <changetw07@gmail.com>
2024-04-14 13:09:20 +0530

    js-control-update

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
 $(document).ready(function(){
 
+// All buttons functions 
+
+var dot = function(){
+
+    $('ul').append('<li class = "list">0</li>');
+
+
+}
+
+var one = function(){
+
+    $('ul').append('<li class = "list">1</li>');
+
+
+}
+
+var two = function(){
+
+    $('ul').append('<li class = "list">2</li>');
+
+
+}
+
+var three = function(){
+
+    $('ul').append('<li class = "list">3</li>');
+
+
+}
+    
+var four = function(){
+
+    $('ul').append('<li class = "list">4</li>');
+
+
+}   
+    
     
 
+$('#dotBtn').on("click", dot);
+$('#oneBtn').on("click", one);
+$('#twoBtn').on("click", two);
+$('#threeBtn').on("click", three);
+$('#fourBtn').on("click", four);
 
 
 
@@ -11,15 +53,4 @@ $(document).ready(function(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-    
 });
