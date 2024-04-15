f01d5754615b007b7da517d2eea15e9f4da47b7c
Nikhil Sajwan <147373447+Nikhil-sajwan007@users.noreply.github.com>
2024-04-15 07:10:30 +0000

    button-functions

--- main.js
+++ main.js
@@ -1,92 +1,113 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-// All buttons functions 
+    // All buttons functions 
 
-runCount = 0;
-
-var dot = function(){
-
-    $('ul').append('<li class = "list">0</li>');
+    runCount = 0;
     
 
-}
 
-var one = function(){
+    var dot = function () {
 
-    $('ul').append('<li class = "list">1</li>');
-    
-    
-    runCount = runCount + 1
-    $('#run-Count').text(runCount)
-  
-
-}
-
-var two = function(){
-
-    $('ul').append('<li class = "list">2</li>');
+        $('ul').append('<li class = "list">0</li>');
 
 
-}
+    }
 
-var three = function(){
+    var one = function () {
 
-    $('ul').append('<li class = "list">3</li>');
-
-
-}
-    
-var four = function(){
-
-    $('ul').append('<li class = "list">4</li>');
+        $('ul').append('<li class = "list">1</li>');
 
 
-}   
-    
-var six = function(){
-
-    $('ul').append('<li class = "list">6</li>');
+        runCount = runCount + 1
+        $('#run-Count').text(runCount)
 
 
-}
-
-var noBall = function(){
-
-    $('ul').append('<li class = "list">NB<sup>+1</sup></li>');
+    }
 
 
-}  
-
-var wideBall = function(){
-
-    $('ul').append('<li class = "list">WB<sup>+1</sup></li>');
 
 
-}
+    var two = function () {
 
-var reset = function(){
-
-    $('ul li').remove()
-}
-
-var undo = function(){
-
-    $('ul li:last-child').remove()
-}
-
-$('#dotBtn').on("click", dot);
-$('#oneBtn').on("click", one);
-$('#twoBtn').on("click", two);
-$('#threeBtn').on("click", three);
-$('#fourBtn').on("click", four);
-$('#sixBtn').on("click", six);
-$('#noBall').on("click", noBall);
-$('#wideBall').on("click", wideBall);
-$('#reset').on("click", reset);
-$('#undo').on("click", undo);
+        $('ul').append('<li class = "list">2</li>');
+        runCount = runCount + 2
+        $('#run-Count').text(runCount)
 
 
-// ALL button function ends here 
+    }
+
+    var three = function () {
+
+        $('ul').append('<li class = "list">3</li>');
+        runCount = runCount + 3;
+        $('#run-Count').text(runCount)
+
+
+    }
+
+    var four = function () {
+
+        $('ul').append('<li class = "list">4</li>');
+        runCount = runCount + 4;
+        $('#run-Count').text(runCount)
+
+
+    }
+
+    var six = function () {
+
+        $('ul').append('<li class = "list">6</li>');
+        runCount = runCount + 6;
+        $('#run-Count').text(runCount)
+
+    }
+
+    var noBall = function () {
+
+        $('ul').append('<li class = "list">NB<sup>+1</sup></li>');
+        runCount = runCount + 1
+        $('#run-Count').text(runCount)
+
+
+    }
+
+    var wideBall = function () {
+
+        $('ul').append('<li class = "list">WB</li>');
+        runCount = runCount + 1;
+        $('#run-Count').text(runCount);
+
+
+    }
+
+    var reset = function () {
+
+        $('ul li').remove()
+    }
+
+    var undo = function () {
+
+        var value = $('ul li:last-child').text();
+
+        runCount = runCount - value;
+        $('#run-Count').text(runCount);
+        $('ul li:last-child').remove();
+        
+    }
+
+    $('#dotBtn').on("click", dot);
+    $('#oneBtn').on("click", one);
+    $('#twoBtn').on("click", two);
+    $('#threeBtn').on("click", three);
+    $('#fourBtn').on("click", four);
+    $('#sixBtn').on("click", six);
+    $('#noBall').on("click", noBall);
+    $('#wideBall').on("click", wideBall);
+    $('#reset').on("click", reset);
+    $('#undo').on("click", undo);
+
+
+    // ALL button function ends here 
 
 
 
